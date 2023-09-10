81557c03199708a28c7791796b33c5757c74c041
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-09-10 23:48:01 +0000

    Deploying to gh-pages from @ titova-polina/goit-react-hw-05-movies@2e1da9e37a791e7c49566b86ecd9ca39c4c8bdd1 🚀

---
+++ static/js/922.f7b0a58d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsxs)("main",{children:[(0,n.jsx)("b",{children:"404"}),(0,n.jsx)("p",{children:"Sorry, we couldn't find that page"})]})}}}]);
+//# sourceMappingURL=922.f7b0a58d.chunk.js.map
